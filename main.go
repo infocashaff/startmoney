87014883d32a66c3a902be1110a73baadbdddb20
infocashaff <147669916+infocashaff@users.noreply.github.com>
2024-07-03 16:29:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/file.pdf", getFile)
+	r.GET("/f.png", getFPng)
+	r.GET("/s.png", getSPng)
 	r.Run()
 }
 
 func getFile(c *gin.Context) {
 	c.File("file.pdf")
+}
+
+func getFPng(c *gin.Context) {
+	c.File("f.png")
+}
+
+func getSPng(c *gin.Context) {
+	c.File("s.png")
 }
